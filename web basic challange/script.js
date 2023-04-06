cc4b6073fa0d5b4d7d774075129ff6a5e1afe7eb
ADAM <110321950+Bayaniadamsasaki@users.noreply.github.com>
2023-04-06 17:11:21 +0800

    Add files via upload

---
+++ web basic challange/script.js
@@ -0,0 +1,22 @@
+// //Navbar Fixed
+// window.onscroll = function() {
+//   const header = document.getElementById('sticky');
+//   const fixedNav = sticky.offsetTop;
+
+//   if (window.pageYOffset > fixedNav) {
+//     header.classList.add('sticky-fixed');
+//   } else {
+//     header.classList.remove('sticky-fixed');
+//   }
+// };
+
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('ul');
+
+// hamburger.addEventListener('click', () => {
+//   hamburger.classList.toggle('open');
+//   navLinks.classList.toggle('show');
+// });
+
+
+
